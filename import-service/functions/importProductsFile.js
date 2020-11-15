57215a5303193e7c2dc85b5372d0920ed531b09e
wid <sortayne@gmail.com>
2020-11-16 00:03:47 +0400

    Added parsing funtion

---
+++ import-service/functions/importProductsFile.js
@@ -0,0 +1,46 @@
+const AWS = require('aws-sdk');
+const BUCKET_NAME = 'my-2nd-app-er-files';
+const BUCKET_PREFIX = 'uploaded';
+
+const importProductsFile = async event => {
+    let result;
+    const headers = {
+      "Access-Control-Allow-Headers" : "Content-Type",
+      "Access-Control-Allow-Origin": "http://my-2nd-app-er.s3-website-us-east-1.amazonaws.com",
+      "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+    };
+  
+    try {
+      const catalogName = event.queryStringParameters.name;
+    
+      const s3 = new AWS.S3({ region: 'us-east-1`' });
+      const params = {
+          Bucket: BUCKET_NAME,
+          Key: `${BUCKET_PREFIX}/${catalogName}`,
+          Expires: 60,
+          ContentType: 'text/csv',
+      };
+  
+      return new Promise((resolve, reject) => {
+        s3.getSignedUrl('putObject', params, (error, url) => {
+          if (error) {
+              reject(error);
+          } else {
+            resolve({
+              statusCode: 200,
+              headers,
+              body: url,
+            });
+          }
+        });
+      });
+    } catch (e) {
+      return {
+        statusCode: 404,
+        headers,
+        body: JSON.stringify(event),
+    }
+    }
+  };
+
+  module.exports = importProductsFile;
